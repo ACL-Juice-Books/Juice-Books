5fe37f7ff792b816dcc6e40529bca811f6e4cc43
Dylan Floyd <floyd800@gmail.com>
2022-01-31 15:37:35 -0800

    change data/setup.js to run all files in sql/

---
+++ __tests__/books.test.js
@@ -0,0 +1,18 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should get a book', async () => {
+    request()
+  });
+});
