062ab5fca68345fcde9bf5bd770ca8f961b47c3c
Estefani <stef45689@gmail.com>
2022-01-31 18:49:47 -0600

    Authors

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -12,7 +12,7 @@ describe('book routes', () => {
     pool.end();
   });
 
-  it('should get a book', async () => {
-    request()
+  it.skip('should get a book', async () => {
+    request();
   });
 });
